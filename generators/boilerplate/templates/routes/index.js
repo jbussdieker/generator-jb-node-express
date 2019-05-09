616021d483f5f7b4b9a1bc43079e881a729bade6
Joshua Bussdieker <jbussdieker@gmail.com>
2019-05-08 19:19:03 -0700

    Guard against missing value

--- generators/boilerplate/templates/routes/index.js
+++ generators/boilerplate/templates/routes/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 /* eslint new-cap: off */
 const router = express.Router();
 const config = require('config');
-const routes = config.get('routes');
 
-Object.keys(routes).forEach((route) => {
-  router.use(`/${route}`, require(`./${route}`));
-})
+if (config.has('routes')) {
+  const routes = config.get('routes');
+
+  Object.keys(routes).forEach((route) => {
+    router.use(`/${route}`, require(`./${route}`));
+  })
+}
 
 module.exports = router;
